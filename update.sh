7a2cd18db91cb240e78bd2574a233c28c6380fa7
Jean Patrick Wilhvock <je4n.pw@gmail.com>
2018-04-10 13:43:46 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 tar xvfJ ixcphpipam.tar.xz ;
-mv logo.png /var/www/phppipam/css/images/ ;
+mv logo.png /var/www/phppipam/css/images/logo/ ;
 mv /var/www/phpipam/app/login/index.php /var/www/phpipam/app/login/index.old ;
 mv index.php /var/www/phpipam/app/login/
