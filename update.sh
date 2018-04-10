6ee88da246dfafc4bf8e9ed248bb28a7667633fc
Jean Patrick Wilhvock <je4n.pw@gmail.com>
2018-04-10 13:35:38 -0300

    Delete update.sh

--- update.sh
+++
@@ -1,5 +0,0 @@
-tar -zxvf ixcphpipam-update.tar.xz ; 
-cd phpipam-update ;
-mv logo.png /var/www/phppipam/css/images/ ; 
-mv /var/www/phpipam/app/login/index.php /var/www/phpipam/app/login/index.old ;
-mv index.php /var/www/phpipam/app/login/
