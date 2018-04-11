dc1c2eabaea890bbbc92c2766c34a189fe19a9f8
Jean Patrick Wilhvock <je4n.pw@gmail.com>
2018-04-11 08:42:08 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -9,9 +9,5 @@ mv footer.php /var/www/phpipam/app/footer.php ;
 echo -e "Limpando a instalação \n"
 rm README.md ;
 rm update.sh ;
-cd .. ;
-rm -rf ixcphpipam-update ;
-cd .. ;
-rm -rf tmp ; 
 echo -e "Pronto. Esse script fez as alterações de customização para o phpipam instalado pela consultoria IXCsoft." ;
 echo -e "Script criado por Je4nPw"
