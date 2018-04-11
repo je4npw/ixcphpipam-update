0650ce5dff0020abca454fc97d6c03d2e2055dfe
Jean Patrick Wilhvock <je4n.pw@gmail.com>
2018-04-11 08:36:57 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ mv logo.png /var/www/phpipam/css/images/logo/ ;
 mv /var/www/phpipam/app/login/index.php /var/www/phpipam/app/login/index.old ;
 mv login-index.php /var/www/phpipam/app/login/index.php ;
 mv /var/www/phpipam/app/footer.php /var/www/phpipam/app/footer.old ;
-mv footer.php /var/www/phpipam/app/ ;
+mv footer.php /var/www/phpipam/app/footer.php ;
 echo -e "Limpando a instalação \n"
 rm README.md ;
 rm update.sh ;
