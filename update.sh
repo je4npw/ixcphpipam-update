16b2da6492d3ebe8881ff81418ff625f7e8a06b3
Jean Patrick Wilhvock <je4n.pw@gmail.com>
2018-04-11 08:27:50 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,13 @@
+echo -e "Descompactar arquivo \n" ;
 tar xvfJ ixcphpipam.tar.xz ;
+echo -e "Movendo arquivos \n"
 mv logo.png /var/www/phpipam/css/images/logo/ ;
 mv /var/www/phpipam/app/login/index.php /var/www/phpipam/app/login/index.old ;
-mv login-index.php /var/www/phpipam/app/login/index.php
+mv login-index.php /var/www/phpipam/app/login/index.php ;
+mv /var/www/phpipam/app/footer.php /var/www/phpipam/app/footer.old ;
+mv footer.php /var/www/phpipam/app/footer.php ;
+echo -e "Limpando a instalação \n"
+rm ixcphpipam.tar.xz ;
+rm logo.png ;
+rm login-index.php ;
+rm footer.php
